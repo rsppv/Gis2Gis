37b8ae12cae149a835ef1c9fc59a41eaa107d46d
rsppv <rsppv.av@gmail.com>
2015-03-03 09:00:28 +0600

    Get Json Response

--- Gis2Gis/Controllers/HomeController.cs
+++ Gis2Gis/Controllers/HomeController.cs
@@ -10,24 +10,42 @@ using Newtonsoft.Json;
 
 namespace Gis2Gis.Controllers
 {
-    public class HomeController : Controller
+  public class HomeController : Controller
+  {
+    // GET: Home
+    [HttpGet]
+    public ActionResult Index()
     {
-        // GET: Home
-        [HttpGet] 
-        public ActionResult Index()
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Index(string query)
+    {
+      string url = ApiSettings.ApiUrl +
+                   @"/search?what=" + query +
+                   "&where=Томск&version=1.3&key=" + ApiSettings.ApiKey;
+      //TODO Надо как то запрос сделать чтоб он вернул Json
+
+      using (var w = new WebClient())
+      {
+        var jsonString = string.Empty;
+        // attempt to download JSON data as a string
+        try
         {
-            return View();
+          jsonString = w.DownloadString(url);
+          
+          ViewBag.Json = jsonString;
+        }
+        catch (Exception e)
+        {
+          Console.WriteLine(e.Message);
+          return null;
         }
 
-      [HttpPost]
-      public ActionResult Index(string query)
-      {
-        string url =  ApiSettings.ApiUrl + 
-                      @"/search?what=" + query +
-                      "&where=Томск&version=1.3&key" + ApiSettings.ApiKey;
-        //TODO Надо как то запрос сделать чтоб он вернул Json
         return View();
       }
-
     }
-}
+
+  }
+} 
---
+++ Gis2Gis/Models/Response.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Gis2Gis.Models
+{
+  public class Response
+    {
+      public string api_version { get; set; }
+      public string response_code { get; set; }
+      public string what { get; set; }
+      public string where { get; set; }
+      public int total { get; set; }
+      public Did_You_Mean did_you_mean { get; set; }
+      public Result[] result { get; set; }
+    }
+
+  public class Did_You_Mean
+  {
+    public Rubric[] rubrics { get; set; }
+    public Geo[] geo { get; set; }
+  }
+
+  public class Rubric
+  {
+    public string name { get; set; }
+  }
+
+  public class Geo
+  {
+    public string id { get; set; }
+    public string name { get; set; }
+  }
+
+  public class Result
+  {
+    public string id { get; set; }
+    public string lon { get; set; }
+    public string lat { get; set; }
+    public string name { get; set; }
+    public Firm_Group firm_group { get; set; }
+    public string hash { get; set; }
+    public string city_name { get; set; }
+    public string address { get; set; }
+    public string[] rubrics { get; set; }
+    public int reviews_count { get; set; }
+  }
+
+  public class Firm_Group
+  {
+    public string id { get; set; }
+    public object count { get; set; }
+  }
+}
